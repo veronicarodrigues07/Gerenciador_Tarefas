fb33490037593e60e928e1bd76cc9c096f183dca
veronicarodrigues07 <veronicaizabellyrodrigues12@gmail.com>
2024-04-29 11:20:40 -0300

    Este é um Gerenciador de tarefas simples desenvolvido em PHP e HTML/CSS. Ele permite que os usuários cadastrem novas tarefas, visualizem a lista de tarefas cadastradas e limpem todas as tarefas de uma vez.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Gerenciador_Tarefas
+
+# Gerenciador de Tarefas
+
+Descrição 📝
+Este é um Gerenciador de tarefas simples desenvolvido em PHP e HTML/CSS. Ele permite que os usuários cadastrem novas tarefas, visualizem a lista de tarefas cadastradas e limpem todas as tarefas de uma vez.
+
+Funcionalidade ⚙️
+
+- **Adicionar Tarefas:** Os usuários podem adicionar novas tarefas inserindo o nome da tarefa e clicando no botão "Cadastrar".
+
+- **Visualizar Lista de Tarefas:** Todas as tarefas cadastradas são exibidas em uma lista abaixo do formulário de adição de tarefas.
+
+- **Limpar Tarefas:** Existe um botão "Limpar Tarefas" que permite que os usuários removam todas as tarefas cadastradas de uma vez.
+
+## Como Usar
+
+1. **Clone o Repositório:** Você pode clonar este repositório em sua máquina local usando o seguinte comando:
+
+```bash
+git clone https://github.com/seu_usuario/seu_repositorio.git
+ 
+
+Tecnologia utilizada ⌨️
+PHP: A linguagem de programação principal utilizada para o desenvolvimento do backend da aplicação. O PHP é utilizado para processar as requisições do cliente, manipular os dados das tarefas e interagir com a sessão do usuário para armazenar as tarefas cadastradas.
+
+HTML/CSS: Para a construção da interface do usuário (UI) foi utilizado HTML para estruturar o conteúdo da página e CSS para estilizar e definir a aparência visual da aplicação.
+
+Essas tecnologias foram escolhidas por sua ubiquidade, facilidade de uso e robustez para construir uma aplicação web simples e funcional para gerenciamento de tarefas.
+
+Tecnologia utilizada ⌨️
+https://www.youtube.com/watch?v=dJ49I-QYYUk&ab_channel=MonolitoPHP- video aula para desenvolver o código 
+
+
+
+
+
+
+
---
+++ gerenciamento-de-tarrefas.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['tasks'])) {
+    $_SESSION['tasks'] = array();
+}
+
+if (isset($_GET['task_name'])) {
+    array_push($_SESSION['tasks'], $_GET['task_name']);
+}
+
+if (isset($_GET['clear'])) {
+    unset($_SESSION['tasks']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tarefa.css">
+    <title>Gerenciador de Tarefas</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Gerenciador de Tarefas</h1>
+        </div>
+        <div class="form">
+            <form action="" method="get">
+                <label for="task_name">Nome da Tarefa</label>
+                <input type="text" name="task_name" placeholder="Nome da Tarefa">
+                <button type="submit">Cadastrar</button>
+            </form>
+        </div>
+        <div class="separator"></div>
+        <div class="list-tasks">
+            <?php
+            if (isset($_SESSION['tasks'])) {
+                echo "<ul>";
+                foreach ($_SESSION['tasks'] as $key => $task) {
+                    echo "<li>$task</li>";
+                }
+                echo "</ul>";
+            }
+            ?>
+            <form action="" method="get">
+                <input type="hidden" name="clear" value="clear">
+                <button type="submit">Limpar Tarefas</button>
+            </form>
+        </div>
+        <div class="footer">
+            <p>Desenvolvido por @nomedodev</p>
+        </div>
+    </div>
+</body>
+</html>
